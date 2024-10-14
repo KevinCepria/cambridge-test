3a808e2e782e9feaf6db15b941c2afec1ecc8ca5
Kevin Cepria <kevin.cepria@gmail.com>
2024-10-15 00:30:06 +0800

    create Button component

---
+++ app/components/Button/Button.tsx
@@ -0,0 +1,40 @@
+import clsx from 'clsx';
+
+import { ButtonProps } from './Button.tyes';
+import { LoadingSpinner } from '../LoadingSpinner/LoadingSpinner';
+
+const Button = (props: ButtonProps) => {
+  const { children, disabled, loading, type = 'info', className, ...rootProps } = props;
+
+  return (
+    <button
+      {...rootProps}
+      className={clsx(
+        'px-5 py-2 text-white rounded-lg text-center relative text overflow-hidden',
+        className,
+        {
+          'bg-blue-500': type === 'info',
+          'bg-red-500': type === 'error',
+          '!bg-slate-500 opacity-50': disabled,
+          'cursor-not-allowed': disabled || loading,
+        },
+      )}
+      disabled={loading || disabled}
+    >
+      {loading && (
+        <div
+          className={clsx('absolute inset-0 flex items-center justify-center', {
+            'bg-blue-500': type === 'info',
+            'bg-red-500': type === 'error',
+            '!bg-slate-500': disabled,
+          })}
+        >
+          <LoadingSpinner />
+        </div>
+      )}
+      {children}
+    </button>
+  );
+};
+
+export default Button;
---
+++ app/components/Button/Button.tyes.ts
@@ -0,0 +1,26 @@
+import { HTMLAttributes } from 'react';
+
+type ButtonType = 'error' | 'info';
+
+export type ButtonProps = {
+ /**
+   * The variant of the button.
+   * @default 'info'
+   */
+ type?: ButtonType;
+
+ /**
+  * If `true`, the button will be disabled.
+  * @default false
+  */
+ disabled?: boolean;
+
+ /**
+  * If `true`, the button will render a loading spinner hiding the provided content.
+  * This property is ignored when `disabled` is true. The loading state also does not allow
+  * the button to be interacted with.
+  * @default false
+  */
+ loading?: boolean;
+
+} & HTMLAttributes<HTMLButtonElement>;
