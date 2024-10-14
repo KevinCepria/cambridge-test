b96f256f2e807614ca760ac9a05c8c4defe1c18b
Kevin Cepria <kevin.cepria@gmail.com>
2024-10-15 01:02:19 +0800

    fix code alignment

--- app/components/Button/Button.types.ts
+++ app/components/Button/Button.types.ts
@@ -3,24 +3,23 @@ import { HTMLAttributes } from 'react';
 type ButtonType = 'error' | 'info';
 
 export type ButtonProps = {
- /**
+  /**
    * The variant of the button.
    * @default 'info'
    */
- type?: ButtonType;
+  type?: ButtonType;
 
- /**
-  * If `true`, the button will be disabled.
-  * @default false
-  */
- disabled?: boolean;
+  /**
+   * If `true`, the button will be disabled.
+   * @default false
+   */
+  disabled?: boolean;
 
- /**
-  * If `true`, the button will render a loading spinner hiding the provided content.
-  * This property is ignored when `disabled` is true. The loading state also does not allow
-  * the button to be interacted with.
-  * @default false
-  */
- loading?: boolean;
-
+  /**
+   * If `true`, the button will render a loading spinner hiding the provided content.
+   * This property is ignored when `disabled` is true. The loading state also does not allow
+   * the button to be interacted with.
+   * @default false
+   */
+  loading?: boolean;
 } & HTMLAttributes<HTMLButtonElement>;
