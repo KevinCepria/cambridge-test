3a780af97f7510d18e88eaac77d86c8c6a953008
Kevin Cepria <kevin.cepria@gmail.com>
2024-10-15 19:24:06 +0800

    create type files for Articles and Users

---
+++ app/types/User.ts
@@ -0,0 +1,22 @@
+export type User = {
+  id: number;
+  name: string;
+  username: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zupcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+};
