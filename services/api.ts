9503184989403527b9494f41364e61b14d76245e
Kevin Cepria <kevin.cepria@gmail.com>
2024-10-16 17:07:29 +0800

    add services folder for api calls

---
+++ services/api.ts
@@ -0,0 +1,25 @@
+import axios, { AxiosResponse } from 'axios';
+
+import { ArticleType } from '@/types/Article';
+
+const API_BASE_URL = 'https://jsonplaceholder.typicode.com';
+
+export const fetchArticles = async () => {
+  const response: AxiosResponse<ArticleType[]> = await axios.get(`${API_BASE_URL}/posts`);
+  return response.data;
+};
+
+export const fetchArticle = async (id: number) => {
+  const response: AxiosResponse<ArticleType> = await axios.get(`${API_BASE_URL}/posts/${id}`);
+  return response.data;
+};
+
+export const createArticle = async (data: any) => {
+  const response: AxiosResponse<ArticleType> = await axios.post(`${API_BASE_URL}/posts`, data);
+  return response.data;
+};
+
+export const updateArticle = async (id: number, data: any) => {
+  const response: AxiosResponse<ArticleType> = await axios.put(`${API_BASE_URL}/posts/${id}`, data);
+  return response.data;
+};
