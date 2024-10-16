be8d0d32dba80bc42cd7199174382d62dd03503a
Kevin Cepria <kevin.cepria@gmail.com>
2024-10-16 21:00:23 +0800

    use logo in login page

--- app/(open)/page.tsx
+++ app/(open)/page.tsx
@@ -2,6 +2,7 @@
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { Field, Input } from '@headlessui/react';
+import { LogoV1 } from '@/icons/logo/LogoV1';
 // import { login } from '../utils/auth';
 
 import { TextInput } from '@/components/TextInput';
@@ -26,8 +27,12 @@ const LoginPage = () => {
 
   return (
     <div className="h-screen flex items-center justify-center">
-      <div className="max-w-md w-full">
-        <form className="border border-gray-400 border-opacity-50 rounded-lg p-10 space-y-5 -mt-10">
+      <div className="-mt-20 max-w-md w-full">
+        <div className="flex justify-center">
+          <LogoV1 className="text-blue-500 h-20 w-72 mb-10" />
+        </div>
+
+        <form className="border border-gray-400 border-opacity-50 rounded-lg p-10 space-y-5">
           <h1 className="text-3xl font-bold uppercase mb-10 text-center">Sign in</h1>
           <TextInput
             type="text"
