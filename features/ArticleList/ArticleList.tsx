e386ce5871883a07cfc900a10e630ead859046eb
Kevin Cepria <kevin.cepria@gmail.com>
2024-10-18 02:40:33 +0800

    fix grid styling in ArticleList component

--- features/ArticleList/ArticleList.tsx
+++ features/ArticleList/ArticleList.tsx
@@ -22,7 +22,7 @@ export const ArticleList = (props: ArticleListProps) => {
     );
   }
   return (
-    <div className="grid gap-5 sm:grid-cols-[repeat(auto-fit,_minmax(350px,_1fr)) grid-cols-[repeat(auto-fit,_minmax(250px,_1fr))] mb-10">
+    <div className="grid gap-5 sm:grid-cols-[repeat(auto-fit,_minmax(350px,_1fr))] grid-cols-[repeat(auto-fit,_minmax(250px,_1fr))] mb-10">
       {articles.map((article) => (
         <Article article={article} key={article.id} />
       ))}
