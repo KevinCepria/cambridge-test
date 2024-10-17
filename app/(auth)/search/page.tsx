9c835f3317f1789dd10cbf87e274d8d428c67258
Kevin Cepria <kevin.cepria@gmail.com>
2024-10-17 22:00:36 +0800

    implement search function in search page

--- app/(auth)/search/page.tsx
+++ app/(auth)/search/page.tsx
@@ -1,5 +1,8 @@
 'use client';
 import { useEffect, useState } from 'react';
+import { PageProps } from '.next/types/app/(auth)/layout';
+import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 
 import { ArticleList } from '@/features/ArticleList';
 import { ArticleType } from '@/types/Article';
@@ -8,23 +11,50 @@ import { SelectInput } from '@/components/SelectInput';
 import { Button } from '@/components/Button';
 import { fetchArticles } from 'services/api';
 
-import { SELECT_OPTIONS_SEARCH } from 'utils/constants';
+import { SELECT_OPTIONS_SEARCH, APP_ROUTES } from 'utils/constants';
 
-const SearchPage = () => {
+const SearchPage = (props: PageProps) => {
+  const searchParams = props.searchParams as Record<string, string>;
   const [articles, setArticles] = useState<ArticleType[]>([]);
 
+  const paramKeys = Object.keys(searchParams);
+  const [paramKey, setParamKey] = useState(paramKeys[0] || SELECT_OPTIONS_SEARCH[0].value);
+  const [paramValue, setParamValue] = useState(searchParams[paramKeys[0]] || '');
+
+  const router = useRouter();
+
   useEffect(() => {
-    fetchArticles().then((data) => setArticles(data));
-  }, []);
+    fetchArticles(null, paramKeys[0], searchParams[paramKeys[0]]).then((data) => setArticles(data));
+  }, [searchParams]);
 
   return (
     <div>
       <h1 className="sm:text-3xl font-bold uppercase sm:mb-10 mb-5 text-xl">Search Articles</h1>
       <div>
         <form className="flex items-stretch justify-end gap-3 mb-5 max-sm:flex-col">
-          <TextInput placeholder="Search..." className="max-sm:w-full" />
-          <SelectInput options={SELECT_OPTIONS_SEARCH} defaultValue={'Title'} />
-          <Button type="submit">Search</Button>
+          <TextInput
+            placeholder="Search..."
+            className="max-sm:w-full"
+            onChange={(e) => setParamValue(e.currentTarget.value)}
+            value={paramValue}
+          />
+          <SelectInput
+            options={SELECT_OPTIONS_SEARCH}
+            value={paramKey}
+            onChange={(e) => setParamKey(e.currentTarget.value)}
+          />
+          <Button
+            type="button"
+            onClick={() =>
+              router.push(
+                `${APP_ROUTES.searchArticles}${
+                  paramKey && paramValue ? `?${paramKey}=${paramValue}` : ''
+                }`,
+              )
+            }
+          >
+            Search
+          </Button>
         </form>
         <ArticleList articles={articles} />
       </div>
