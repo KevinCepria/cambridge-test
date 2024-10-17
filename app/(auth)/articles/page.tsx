b922f573d1cdb01d0c45e71076cfb36c5c4dd63c
Kevin Cepria <kevin.cepria@gmail.com>
2024-10-17 08:09:29 +0800

    add create Article button in Articles page

--- app/(auth)/articles/page.tsx
+++ app/(auth)/articles/page.tsx
@@ -1,11 +1,16 @@
 'use client';
+import { useEffect, useState } from 'react';
+
 import { ArticleList } from '@/features/ArticleList';
 import { ArticleType } from '@/types/Article';
-import { useEffect, useState } from 'react';
+import { PlusIcon } from '@/icons/solid';
+import { Button } from '@/components/Button';
+import { ArticleModalForm } from '@/features/ArticleModalForm';
 import { fetchArticles } from 'services/api';
 
 const ArticlesPage = () => {
   const [articles, setArticles] = useState<ArticleType[]>([]);
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
     fetchArticles().then((data) => setArticles(data));
@@ -14,7 +19,20 @@ const ArticlesPage = () => {
   return (
     <div>
       <h1 className="text-3xl font-bold uppercase mb-10">My Articles</h1>
-      <ArticleList articles={articles} />
+      <div>
+        <div className="flex items-stretch justify-end gap-3 mb-5">
+          <Button
+            onClick={() => setOpen(true)}
+            type="button"
+            title="Create new article"
+            className="!p-4"
+          >
+            <PlusIcon className="font-bold" />
+          </Button>
+          <ArticleModalForm open={open} onClose={() => setOpen(false)} />
+        </div>
+        <ArticleList articles={articles} />
+      </div>
     </div>
   );
 };
