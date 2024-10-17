3741a6de521d8e3512ac7b21ac41fed2cb14d26b
Kevin Cepria <kevin.cepria@gmail.com>
2024-10-17 15:52:04 +0800

    add bottom margin to ArticleList component

--- features/ArticleList/ArticleList.tsx
+++ features/ArticleList/ArticleList.tsx
@@ -5,7 +5,7 @@ export const ArticleList = (props: ArticleListProps) => {
   const { articles } = props;
 
   return (
-    <div className="grid gap-5 sm:grid-cols-[repeat(auto-fit,_minmax(350px,_1fr)) grid-cols-[repeat(auto-fit,_minmax(250px,_1fr))]">
+    <div className="grid gap-5 sm:grid-cols-[repeat(auto-fit,_minmax(350px,_1fr)) grid-cols-[repeat(auto-fit,_minmax(250px,_1fr))] mb-10">
       {articles.map((article) => (
         <Article article={article} key={article.id} />
       ))}
