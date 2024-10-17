7e8c373245c5c06c623616edd7f7b8f93d6af4c0
Kevin Cepria <kevin.cepria@gmail.com>
2024-10-17 11:17:21 +0800

    add route protection for pages

--- app/(open)/layout.tsx
+++ app/(open)/layout.tsx
@@ -1,9 +1,17 @@
+'use client';
+
 import '@/styles/global.css';
+import { useAuth } from '@/hooks/useAuth';
+import { isNil } from 'lodash';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const { authenticated } = useAuth(true);
+
   return (
     <html lang="en">
-      <body className="antialiased">{children}</body>
+      <body className="antialiased">
+        {!authenticated && !isNil(authenticated) ? children : null}
+      </body>
     </html>
   );
 }
