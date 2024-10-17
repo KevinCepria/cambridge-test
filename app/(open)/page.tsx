65251134b54e346b8f416feeefd0277d97870160
Kevin Cepria <kevin.cepria@gmail.com>
2024-10-17 08:11:46 +0800

    update login page to use react-hook-forms

--- app/(open)/page.tsx
+++ app/(open)/page.tsx
@@ -1,32 +1,32 @@
 'use client';
-import { useState } from 'react';
+
 import { useRouter } from 'next/navigation';
-import { Field, Input } from '@headlessui/react';
+import { useForm, Controller } from 'react-hook-form';
+
 import { LogoV1 } from '@/icons/logo/LogoV1';
 // import { login } from '../utils/auth';
-
+import { LoginType } from '@/types/Login';
 import { TextInput } from '@/components/TextInput';
 import { PasswordInput } from '@/components/PasswordInput';
-
 import { Button } from '@/components/Button';
 
 const LoginPage = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-
   const router = useRouter();
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      username: '',
+      password: '',
+    } as LoginType,
+  });
 
-  //   const handleSubmit = (e: React.FormEvent) => {
-  //     e.preventDefault();
-  //     if (login(username, password)) {
-  //       router.push('/articles');
-  //     } else {
-  //       alert('Invalid login');
-  //     }
-  //   };
+  const onSubmit = (data: LoginType) => console.log('ssss', data);
 
   return (
-    <div className="h-screen flex items-center justify-center">
+    <div className="h-screen flex items-center justify-center" onSubmit={handleSubmit(onSubmit)}>
       <div className="-mt-20 max-w-md w-full">
         <div className="flex justify-center">
           <LogoV1 className="text-blue-500 h-20 w-72 mb-10" />
@@ -34,19 +34,31 @@ const LoginPage = () => {
 
         <form className="border border-gray-400 border-opacity-50 rounded-lg p-10 space-y-5">
           <h1 className="text-3xl font-bold uppercase mb-10 text-center">Sign in</h1>
-          <TextInput
-            type="text"
-            //   value={username}
-            //   onChange={(e) => setUsername(e.target.value)}
-            placeholder="Username"
-            className="w-full"
+          <Controller
+            name="username"
+            control={control}
+            rules={{ required: true }}
+            render={({ field }) => (
+              <TextInput
+                {...field}
+                placeholder="Username"
+                className="w-full"
+                error={errors.username && 'This field is required'}
+              />
+            )}
           />
-          <PasswordInput
-            type="password"
-            //   value={password}
-            //   onChange={(e) => setPassword(e.target.value)}
-            placeholder="Password"
-            className="w-full"
+          <Controller
+            name="password"
+            control={control}
+            rules={{ required: true }}
+            render={({ field }) => (
+              <PasswordInput
+                {...field}
+                placeholder="Password"
+                className="w-full"
+                error={errors.password && 'This field is required'}
+              />
+            )}
           />
           <Button type="submit" className="w-full font-bold">
             Login
